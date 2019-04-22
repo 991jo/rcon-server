fcbf8677d95c90a1c605b1e3440615eb70470a3c
Johannes Erwerle <johannes.erwerle@googlemail.com>
2019-04-22 22:31:36 +0200

    added rcon_server and rcon_connection

---
+++ rcon_server/rcon_connection.py
@@ -0,0 +1,190 @@
+import asyncio
+import logging
+
+from rcon_packet import RCONPacket
+
+logger = logging.getLogger(name="RCONServer")
+
+class RCONConnection(asyncio.Protocol):
+    def __init__(self, rcon_server):
+        """Initializes a new connection with a client.
+
+        :param rcon_server: The RCONServer which handles the commands
+        :param conn: the connection (tcp socket)
+        """
+        logger.info("started initialization of a RCON Connection")
+        self._rcon_server = rcon_server
+        self._state = "unauthenticated"
+
+        # State machine for the connection:
+        # 1. "unauthenticated
+        # 2. "authenticated
+        # 3. "closed"
+        # State transitions: 1 -> 2 -> 3
+
+        self._buffer = b"" # buffer for the data received from the connection
+        self._running = True
+        self._transport = None
+
+        # append the RCONConnection to the RCONServer Connections
+        self._rcon_server.connections.append(self)
+
+    def connection_made(self, transport):
+        """This method is called when a client connects and the transport
+        is initialized. Transport is a TCP connection in this case."""
+        logger.info("connection made")
+        self._transport = transport
+
+    def connection_lost(self, exc):
+        """This method is called when the transport is closed.
+        If *exc* is None either the connection has received a regular EOF or
+        the connection was closed from this side. If *exc* is an Exception
+        the other side has closed the connection not orderly."""
+        self._state = "closed"
+
+    def data_received(self, data):
+        """This method is called when the socket has received data."""
+        # check if the state of the RCON connection is not closed
+        if self._state != "closed":
+            self._buffer += data
+            # check if packet is complete
+            packet, buffer = RCONPacket.from_buffer(self._buffer)
+            if packet is not None:
+                self._buffer = buffer
+                self._handle_packet(packet)
+        else:
+            self._transport.close()
+
+    def eof_received(self):
+        """This method is called when the other side has closed its connection.
+        :return: False because the transport may close itself.
+        """
+        self._state = "closed"
+
+    def close_connection(self):
+        """
+        Closes the connection. This is used when the password is written
+        or the client sends an invalid command. This also closes the underlying
+        transport.
+        """
+        logger.info("closing")
+        self._state = "closed"
+        self._transport.close()
+
+    def _handle_packet(self, packet):
+        """
+        Handles the received packet.
+        :param packet: a RCONPacket
+        """
+        logger.info(f"received packet {packet!r}")
+
+        # Handling of empty SERVERDATA_RESPONSE_VALUE packages precedes everything.
+        if (self._state != "closed"
+                and packet.body == ""
+                and packet.type == RCONPacket.SERVERDATA_RESPONSE_VALUE):
+            logger.info("empty packet received")
+            self._handle_empty_response_value(packet)
+            return
+
+        if self._state == "unauthenticated":
+            # First packet needs to be a authentication packet
+            if packet.type == RCONPacket.PACKET_TYPES["SERVERDATA_AUTH"]:
+                if self._rcon_server.check_password(packet.body):
+                    self._handle_correct_login(packet)
+                else:
+                    logger.info("incorrect login received")
+                    self._handle_incorrect_login(packet)
+            else:
+                #invalid packet, close connection?
+                self.close_connection()
+
+        elif self._state == "authenticated":
+            # only valid packet shoud be an SERVERDATA_EXECCOMMAND
+            if packet.type == RCONPacket.PACKET_TYPES["SERVERDATA_EXECCOMMAND"]:
+                self._rcon_server.handle_execcommand(packet, self)
+            else:
+                #invalid packet, close connection?
+                self.close_connection()
+
+        elif self._state == "closed":
+            # no packets are valid here, close the connection
+            self.close_connection()
+
+        # do something when a fallthrough happens
+
+    def _handle_empty_response_value(self, packet):
+        """
+        This method handles empty SERVERDATA_RESPONSE_VALUE packets.
+        According to [https://developer.valvesoftware.com/wiki/Source_RCON_Protocol#Multiple-packet_Responses]
+        first an empty SERVERDATA_RESPONSE_VALUE packet is send, followed by
+        an other SERVERDATA_RESPONSE_VALUE packet with "0x0000 0001 0000 0000"
+        as the body.
+
+        :param packet: a RCONPacket with type == SERVERDATA_RESPONSE_VALUE and
+        body == ""
+        """
+        assert packet.type == RCONPacket.SERVERDATA_RESPONSE_VALUE
+        assert packet.body == ""
+
+        first_packet = RCONPacket(packet.id,
+                                  RCONPacket.PACKET_TYPES["SERVERDATA_RESPONSE_VALUE"],
+                                  body="")
+        second_packet = RCONPacket(packet.id,
+                                   RCONPacket.PACKET_TYPES["SERVERDATA_RESPONSE_VALUE"],
+                                   body="\x00\x00\x00\x01\x00\x00\x00\x00")
+        self.send_packet(first_packet)
+        self.send_packet(second_packet)
+
+    def _handle_correct_login(self, packet):
+        """
+        Called when a correct login happens.
+        Sends an empty SERVERDATA_RESPONSE_VALUE and an empty
+        SERVERDATA_AUTH_RESPONSE, both with a id matching to the initial packet.
+        Changes the state of the connection to authenticated.
+        """
+
+        id = packet.id
+        # send empty SERVERDATA_RESPONSE_VALUE
+        response_value = RCONPacket(id, RCONPacket.PACKET_TYPES["SERVERDATA_RESPONSE_VALUE"],
+                                    "")
+        # send SERVERDATA_AUTH_RESPONSE
+        auth_response = RCONPacket(id, RCONPacket.PACKET_TYPES["SERVERDATA_AUTH_RESPONSE"],
+                                   "")
+        self.send_packet(response_value)
+        self.send_packet(auth_response)
+        self._state = "authenticated"
+        logger.info("connection authenticated")
+
+    def _handle_incorrect_login(self, packet):
+        """
+        Called when a incorrect login happens.
+        Sends an empty SERVERDATA_RESPONSE_VALUE packet with a matching id and
+        a SERVERDATA_AUTH_RESPONSE with -1 as id.
+        """
+
+        id = packet.id
+        # send empty SERVERDATA_RESPONSE_VALUE
+        response_value = RCONPacket(id, RCONPacket.PACKET_TYPES["SERVERDATA_RESPONSE_VALUE"],
+                                    "")
+        # send a SERVERDATA_AUTH_RESPONSE with id=-1
+        auth_response = RCONPacket(-1, RCONPacket.PACKET_TYPES["SERVERDATA_AUTH_RESPONSE"],
+                                   "")
+        self.send_packet(response_value)
+        self.send_packet(auth_response)
+        logger.warning("incorrect authentication")
+
+    @property
+    def state(self):
+        """
+        :return: the state of the connection.
+        It is either "unauthenticated", "authenticated" or "closed"
+        """
+        return self._state
+
+    def send_packet(self, packet):
+        """
+        Sends the given packet.
+        :param packet: a RCONPacket.
+        """
+        logger.info(f"sending packet {packet!r}")
+        self._transport.write(packet.msg())
---
+++ rcon_server/rcon_server.py
@@ -0,0 +1,80 @@
+import asyncio
+import logging
+
+from rcon_connection import RCONConnection
+
+logger = logging.getLogger(name="RCONServer")
+logger.setLevel(logging.DEBUG)
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.DEBUG)
+logger.addHandler(console_handler)
+
+class RCONServer:
+    def __init__(self, bind=("localhost",27015), password=None):
+        """Initializes a RCON Server.
+        :param bind: a tuple (address, port) to bind to
+        :password: the RCON password
+        """
+        self.connections = [] # list of all connections
+                              # the connections append and remove themselves
+                              # to/from it
+        self.set_password(password)
+        self.bind = bind
+
+    @property
+    def password(self):
+        """:return: The RCON password as str"""
+        return self._password
+
+    def connection_factory(self):
+        conn = RCONConnection(self)
+        return conn
+
+    def check_password(self, pw):
+        """
+        :return: True if the given password *pw* matches the RCON password.
+        False otherwise or if the RCON password is the empty string or None.
+        """
+        if self.password == "" or self.password is None:
+            return False
+        return self.password == pw
+
+    def set_password(self, password):
+        """Used to set the RCON password.
+        If this function is called all authenticated connections are closed.
+        :param password: the new rcon password.
+        """
+        if password is None or isinstance(password,str):
+            self._password = password
+        else:
+            raise ValueError("password needs to be a string or None.")
+        for conn in self.connections:
+            if conn.state != "unauthenticated":
+                conn.close_connection()
+
+    async def listen(self):
+        """Starts listening on the socket and handling requests."""
+
+        self._loop = asyncio.get_event_loop()
+        server = await self._loop.create_server(self.connection_factory,
+                                          self.bind[0], self.bind[1])
+        print(server)
+        async with server:
+            logger.info("starting server")
+            for socket in server.sockets:
+                logger.info("listening on %s:%s" % socket.getsockname())
+            await server.serve_forever()
+
+    def handle_execcommand(self, packet, connection):
+        """
+        Handles an EXECCOMMAND package. This command has to be implemented by
+        a subclass of the RCONServer.
+        :param packet: the packet containing the command
+        :param connection: the RCONConnection which calls this method
+        """
+        raise NotImplementedError("This method is not implemented! A subclass "
+                                  "must implement this method!")
+
+if __name__ == "__main__":
+    server = RCONServer()
+    asyncio.run(server.listen())
