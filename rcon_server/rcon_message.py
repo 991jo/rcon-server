4b8272f5be2a184e98ac05345a225805a650d10f
Johannes Erwerle <johannes.erwerle@googlemail.com>
2019-05-12 16:40:53 +0200

    expanded rcon_message and tests

--- rcon_server/rcon_message.py
+++ rcon_server/rcon_message.py
@@ -5,16 +5,59 @@ class RCONMessage:
     A RCONMessage represents a message which may consist of multiple packets.
     """
 
-    def __init__(self, packet=[]):
+    def __init__(self, packet=None, id=None, type=None, body=None):
         """
-        creates a new RCONMesage object.
+        creates a new RCONMessage object.
 
-        :param packet: a RCONPacket or a list of RCONPackets. May be empty.
+        A RCONMessage is a higher abstraction from RCONPackets.
+        It has an id, type and a body as a RCONPacket has.
+        But the body can be longer than the maximum size of a single
+        RCONPacket.
+
+        This can either be created from a packets or by giving an
+        *id*, *type* and *body*.
+
+        If build from packes then all packets need to have the same type
+        and id.
+
+        :param packet: a RCONPacket or a list of RCONPackets.
+        :param id: int, the id of the packet
+        :param type: int, the type of the packet
+        :param body: str, the body of the message, a regular python string,
+        not a bytearray.
         """
-        if isinstance(packet, RCONPacket):
-            packet = [packet]
-        assert isinstance(packet, list)
-        self.packets = packet
+        if packet is not None:  # build the message from packets
+
+            # other parameters should not be set
+            assert id is None, "id parameter should not be set if packet is given"
+            assert type is None, "type parameter should not be set if packet is given"
+            assert body is None, "body parameter should not be set if packet is given"
+
+            # make sure that packet is a list
+            if isinstance(packet, RCONPacket):
+                packet = [packet]
+
+            assert len(packet) != 0, "packet has to be a RCONPacket or"\
+                                     "an non empty list of RCONPackets"
+
+            # check if all packets have the same type and id
+            type = packet[0].type
+            id = packet[0].id
+
+            for i, p in enumerate(packet[1:]):
+                assert p.type == type, "type of packet 0 %s and packet %d %s dont match" % (packet[0], i, p)
+                assert p.id == id , "id of packet 0 %s and packet %d %s dont match" % (packet[0], i, p)
+
+            self.packets = packet
+
+        else:  # build the message from id, type, body
+            assert id is not None, "id parameter should be set if no packet is given"
+            assert type is not None, "type parameter should be set if no packet is given"
+            assert body is not None, "body parameter should be set if no packet is given"
+
+            self.packets = [RCONPacket(id=id, type=type, body="")]
+            self.body = body  # call the body via the property to make sure that
+                              # it is split over multiple packets
 
     def add_packet(self, packet):
         """
@@ -23,15 +66,37 @@ class RCONMessage:
         :param packet: a RCONPacket.
         """
         assert isinstance(packet, RCONPacket)
+
         self.packets.append(packet)
 
     def __iter__(self):
-        """Returns an iterator over the packets."""
+        """Returns an iterator over the packets.
+        The iterator has always at least one packet (which may be empty)."""
         return iter(self.packets)
 
     def msg(self):
         """Return the whole message as a bytearray."""
         return b"".join(p.msg() for p in self.packets)
+
+    @property
+    def id(self):
+        """The id of the RCONPackets"""
+        return self.packets[0].id
+
+    @id.setter
+    def id(self, value):
+        for packet in self.packets:
+            packet.id = value
+
+    @property
+    def type(self):
+        """The type of the RCONPackets"""
+        return self.packets[0].type
+
+    @type.setter
+    def type(self, value):
+        for packet in self.packets:
+            packet.type = value
 
     @property
     def body(self):
@@ -40,3 +105,29 @@ class RCONMessage:
         together.
         """
         return "".join(p.body for p in self.packets)
+
+    @body.setter
+    def body(self, value):
+        """
+        sets the body of the RCONMessage.
+        This may lead to adding/removing packets to/from the message.
+        """
+        # maximum packet size is 4096 - 10 bytes (id, type, 2x terminator)
+        max_size = 4086
+
+        id = self.packets[0].id
+        type = self.packets[0].type
+
+        packets = list()
+        while True:
+            next_size = min(max_size, len(value))
+            packets.append(RCONPacket(id=id, type=type, body=value[0:next_size]))
+            value = value[next_size:]
+            if len(value) == 0:
+                break
+        self.packets = packets
+
+    @property
+    def size(self):
+        """The sum of all packets in this message."""
+        return sum(p.size for p in self.packets)
--- rcon_server/test_rcon_message.py
+++ rcon_server/test_rcon_message.py
@@ -9,17 +9,24 @@ class RCONMessageTest(unittest.TestCase):
         self.packet1 = RCONPacket(id=1,
                                       type=RCONPacket.SERVERDATA_RESPONSE_VALUE,
                                       body="test1")
-        self.packet2 = RCONPacket(id=2,
+        self.packet2 = RCONPacket(id=1,
                                       type=RCONPacket.SERVERDATA_RESPONSE_VALUE,
                                       body="test2")
-        self.packet3 = RCONPacket(id=3,
+        self.packet3 = RCONPacket(id=1,
                                       type=RCONPacket.SERVERDATA_RESPONSE_VALUE,
                                       body="test3")
+        self.empty_message = RCONMessage(id=0,
+                                         type=RCONPacket.SERVERDATA_AUTH,
+                                         body="")
+        self.empty_packet = RCONPacket(id=0,
+                                       type=RCONPacket.SERVERDATA_AUTH,
+                                       body="")
+        self.very_long_body = "a"*5000
 
     def test_init_empty(self):
-        """Tests the empty constructor."""
-        message = RCONMessage()
-        self.assertEqual(message.packets, list())
+        """Tests the empty constructor. an completely empty constructor is invalid."""
+        with self.assertRaises(AssertionError):
+            message = RCONMessage()
 
     def test_init_single_packet(self):
         """Tests with a single packet."""
@@ -45,11 +52,11 @@ class RCONMessageTest(unittest.TestCase):
 
     def test_iter_empty(self):
         """Tests the empty iterator."""
-        message = RCONMessage()
         i = 0
-        for _ in message:
+        for p in self.empty_message:
             i += 1
-        self.assertEqual(i,0)
+            self.assertEqual(p.body, "")
+        self.assertEqual(i,1)
 
     def test_iter(self):
         """Test the iterator with some elements."""
@@ -60,9 +67,8 @@ class RCONMessageTest(unittest.TestCase):
         self.assertEqual(i,2)
 
     def test_msg_empty(self):
-        """Tests the msg function without packets."""
-        message = RCONMessage()
-        self.assertEqual(message.msg(), b"")
+        """Tests the msg function an empty packet."""
+        self.assertEqual(self.empty_message.msg(), self.empty_packet.msg())
 
     def test_msg_one_packet(self):
         """Tests the msg function with exactly one packet."""
@@ -76,15 +82,83 @@ class RCONMessageTest(unittest.TestCase):
 
     def test_body_empty(self):
         """Tests the body with an empty message."""
-        message = RCONMessage()
-        self.assertEqual("", message.body)
+        self.assertEqual("", self.empty_message.body)
 
-    def test_body_empty(self):
+    def test_body_one_packet(self):
         """Tests the body with an one packet."""
         message = RCONMessage(self.packet1)
         self.assertEqual("test1", message.body)
 
-    def test_body_empty(self):
+    def test_body_multiple_packets(self):
         """Tests the body with multiple packets."""
         message = RCONMessage([self.packet1, self.packet2])
         self.assertEqual("test1test2", message.body)
+
+    def test_body_setter_single_packet(self):
+        """Test the body setter with a data for a single packet"""
+        message = RCONMessage(id=0, type=RCONPacket.SERVERDATA_AUTH, body="test")
+        self.assertEqual("test", message.body)
+        self.assertEqual(len(message.packets), 1)
+
+    def test_body_setter_multiple_packets(self):
+        """Test the body setter with a data for multiple packets"""
+        message = RCONMessage(id=0,
+                              type=RCONPacket.SERVERDATA_AUTH,
+                              body=self.very_long_body)
+        self.assertEqual(self.very_long_body, message.body)
+        self.assertEqual(len(message.packets), 2)
+
+    def test_id_getter(self):
+        """Tests the getter for the id."""
+        self.assertEqual(self.empty_message.id, self.empty_packet.id)
+
+    def test_id_setter_single_packet(self):
+        """Tests the setter for the id with a single packet."""
+        message = RCONMessage(id=0, type=RCONPacket.SERVERDATA_AUTH, body="test")
+        self.assertEqual(message.id, 0)
+        message.id = 1
+        self.assertEqual(message.id, 1)
+
+    def test_id_setter_multiple_packets(self):
+        """Tests the setter for the id with multiple packets."""
+        message = RCONMessage(id=0,
+                              type=RCONPacket.SERVERDATA_AUTH,
+                              body=self.very_long_body)
+        self.assertEqual(message.id, 0)
+        for packet in message.packets:
+            self.assertEqual(packet.id, 0)
+
+        message.id = 1
+        self.assertEqual(message.id, 1)
+        for packet in message.packets:
+            self.assertEqual(packet.id, 1)
+
+    def test_type_getter(self):
+        """Test the type getter"""
+        self.assertEqual(self.empty_message.type, RCONPacket.SERVERDATA_AUTH)
+
+    def test_type_setter_single_packet(self):
+        """Test the type setter with data for a single packet"""
+        message = RCONMessage(id=0, type=RCONPacket.SERVERDATA_AUTH, body="test")
+        self.assertEqual(message.type, RCONPacket.SERVERDATA_AUTH)
+        message.type = RCONPacket.SERVERDATA_AUTH_RESPONSE
+        self.assertEqual(message.type, RCONPacket.SERVERDATA_AUTH_RESPONSE)
+
+    def test_type_setter_multiple_packets(self):
+        """Test the type setter with data for a multiple packets"""
+        message = RCONMessage(id=0,
+                              type=RCONPacket.SERVERDATA_AUTH,
+                              body=self.very_long_body)
+        self.assertEqual(message.type, RCONPacket.SERVERDATA_AUTH)
+        for packet in message.packets:
+            self.assertEqual(packet.type, RCONPacket.SERVERDATA_AUTH)
+        message.type = RCONPacket.SERVERDATA_AUTH_RESPONSE
+        self.assertEqual(message.type, RCONPacket.SERVERDATA_AUTH_RESPONSE)
+        for packet in message.packets:
+            self.assertEqual(packet.type, RCONPacket.SERVERDATA_AUTH_RESPONSE)
+
+    def test_size_getter_single_packet(self):
+        pass
+
+    def test_size_getter_multiple_packet(self):
+        pass
